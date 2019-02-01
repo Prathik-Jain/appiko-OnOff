a06d78916ccf939d0086e34af78bebeaf813336b
Prathik Jain <prathik862pj@live.com>
2019-02-01 21:37:59 +0530

    :bug: App exits on Back button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform, NavController } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
   constructor(
+    private navController: NavController,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -21,6 +22,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+
+    // Exit app when backbutton is clicked
+    this.platform.backButton.subscribe(() => {
+      navigator["app"].exitApp();
     });
   }
 }
